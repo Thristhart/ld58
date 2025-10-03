883c83895883202cad5390b000b39a735e79ce1e
Tom Shea <tom@tomshea.dev>
2025-10-03 16:53:08 -0700

    draw a grid

---
+++ src/camera.ts
@@ -0,0 +1,1 @@
+export const camera = { x: 0, y: 0, scale: parseFloat(localStorage.getItem("scale") ?? "1") };
--- src/render.ts
+++ src/render.ts
@@ -1,3 +1,6 @@
+import { camera } from "./camera";
+import { GRID_SQUARE_SIZE } from "./constants";
+
 const frameDurations: number[] = new Array();
 const frameDurationsSampleCount = 20;
 let frameTimeIndex = 0;
@@ -7,7 +10,36 @@ export function drawFrame(dt: number, canvas: HTMLCanvasElement, context: Canvas
     frameTimeIndex %= frameDurationsSampleCount;
     const averageFrameTime = frameDurations.reduce((prev, current) => prev + current) / frameDurations.length;
 
-    context.fillStyle = "black";
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.save();
+    context.translate(
+        Math.round((canvas.width / 2 - camera.x - GRID_SQUARE_SIZE / 2) * camera.scale),
+        Math.round((canvas.height / 2 - camera.y - GRID_SQUARE_SIZE / 2) * camera.scale)
+    );
+    context.scale(camera.scale, camera.scale);
+
+    // temporary grid lines
+
+    context.strokeStyle = "black";
+
+    for (let x = -100; x < 100; x++) {
+        context.beginPath();
+        context.moveTo(x * GRID_SQUARE_SIZE, -1000);
+        context.lineTo(x * GRID_SQUARE_SIZE, 1000);
+        context.closePath();
+        context.stroke();
+    }
+    for (let y = -100; y < 100; y++) {
+        context.beginPath();
+        context.moveTo(-1000, y * GRID_SQUARE_SIZE);
+        context.lineTo(1000, y * GRID_SQUARE_SIZE);
+        context.closePath();
+        context.stroke();
+    }
+
+    context.restore();
+
+    context.fillStyle = "black";
     context.fillText(`${Math.round(1000 / averageFrameTime)} FPS`, 32, 32);
 }
