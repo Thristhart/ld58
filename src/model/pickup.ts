66ed67ec65c12ecc155bdbeab329a83a4163c135
Tom Shea <tom@tomshea.dev>
2025-10-04 08:29:46 -0700

    pickup placeholder image. it's a gold coin, kind of?

--- src/model/pickup.ts
+++ src/model/pickup.ts
@@ -1,5 +1,10 @@
 import { Player } from "./entities/player";
 import { Entity } from "./entity";
+import { loadImage } from "#src/images.ts";
+
+import pickupImageUrl from "#src/assets/pickup.png";
+import { GRID_SQUARE_SIZE } from "#src/constants.ts";
+const pickupImage = loadImage(pickupImageUrl);
 
 export class Pickup extends Entity {
     consume(consumer: Entity) {
@@ -10,5 +15,14 @@ export class Pickup extends Entity {
         const newPos = this.gameWorld.getRandomEmptyPositionNearPlayer();
         const newCopy = new Pickup(newPos, this.gameWorld);
         this.gameWorld.addEntity(newCopy);
+    }
+    draw(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement): void {
+        context.drawImage(
+            pickupImage,
+            this.position.x * GRID_SQUARE_SIZE,
+            this.position.y * GRID_SQUARE_SIZE,
+            GRID_SQUARE_SIZE,
+            GRID_SQUARE_SIZE
+        );
     }
 }
